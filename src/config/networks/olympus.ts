aedbdb4c486fd0253f0505422641560923233f69
Enrique <eabz@polispay.org>
2021-08-31 23:36:01 -0500

    Update olympus.ts

--- src/config/networks/olympus.ts
+++ src/config/networks/olympus.ts
@@ -11,7 +11,7 @@ import {
 
 const baseConfig: EnvironmentSettings = {
   clientGatewayUrl: 'https://safe-client.polis.tech/v1',
-  txServiceUrl: 'https://safe-tx.polis.tech/api/v1',
+  txServiceUrl: 'https://safe-txs.polis.tech/api/v1',
   safeUrl: 'https://safe.polis.tech/app',
   gasPrice: 1e9,
   rpcServiceUrl: 'https://rpc.polis.tech',
